1f3eb46459f482e95e8c394ff78016e0d8d8eed7
dosazuwa <dsazuwa@gmail.com>
2023-07-03 23:44:05 +0100

    test:(user.route): retrieve user route

--- tests/integration/users.route.test.ts
+++ tests/integration/users.route.test.ts
@@ -4,7 +4,7 @@ import { request } from '../utils/supertest.helper';
 
 const BASE_URL = '/api/users';
 
-describe('User Verification', () => {
+describe('User Routes', () => {
   const data = {
     firstName: 'Joyce',
     lastName: 'Doe',
@@ -20,54 +20,74 @@ describe('User Verification', () => {
     token = user.generateJWT();
   });
 
-  it('POST /me/verify should create a new verification token', async () => {
-    await request.post(`${BASE_URL}/me/verify`).auth(token, { type: 'bearer' }).expect(200);
+  describe('CRUD User', () => {
+    it('GET /me should return basic user information', async () => {
+      await request
+        .get(`${BASE_URL}/me`)
+        .auth(token, { type: 'bearer' })
+        .expect(200)
+        .then((response) => {
+          expect(response.body.user).toBeDefined();
+          const user = response.body.user;
+
+          expect(user.firstName).toEqual(data.firstName);
+          expect(user.lastName).toEqual(data.lastName);
+          expect(user.email).toEqual(data.email);
+          expect(user.password).not.toBeDefined();
+        });
+    });
   });
 
-  it('PUT /me/verify/:code should fail on invalid code', async () => {
-    await request
-      .put(`${BASE_URL}/me/verify/evrecercrcode`)
-      .auth(token, { type: 'bearer' })
-      .expect(400)
-      .then((response) => {
-        expect(response.body.message).toEqual('Invalid code');
-      });
-  });
-
-  it('PUT /me/verify/:code should fail on expired code', async () => {
-    await Token.destroy({ where: { userId: user.id, type: 'verify' } });
-
-    const code = await Token.create({
-      userId: user.id,
-      type: 'verify',
-      code: Token.generateCode(),
-      expiresAt: new Date(),
+  describe('User Verification', () => {
+    it('POST /me/verify should create a new verification token', async () => {
+      await request.post(`${BASE_URL}/me/verify`).auth(token, { type: 'bearer' }).expect(200);
     });
 
-    await request
-      .put(`${BASE_URL}/me/verify/${code.code}`)
-      .auth(token, { type: 'bearer' })
-      .expect(400)
-      .then((response) => {
-        expect(response.body.message).toEqual('Invalid code');
-      });
-  });
-
-  it('PUT /me/verify/:code should verify user ', async () => {
-    await Token.destroy({ where: { userId: user.id, type: 'verify' } });
-
-    const code = await user.createToken({
-      type: 'verify',
-      code: Token.generateCode(),
-      expiresAt: Token.getExpiration(),
+    it('PUT /me/verify/:code should fail on invalid code', async () => {
+      await request
+        .put(`${BASE_URL}/me/verify/evrecercrcode`)
+        .auth(token, { type: 'bearer' })
+        .expect(400)
+        .then((response) => {
+          expect(response.body.message).toEqual('Invalid code');
+        });
     });
 
-    await request
-      .put(`${BASE_URL}/me/verify/${code.code}`)
-      .auth(token, { type: 'bearer' })
-      .expect(200);
+    it('PUT /me/verify/:code should fail on expired code', async () => {
+      await Token.destroy({ where: { userId: user.id, type: 'verify' } });
 
-    const retrievedUser = await User.findOne({ where: { email: data.email } });
-    expect(retrievedUser?.status).toEqual('active');
+      const code = await Token.create({
+        userId: user.id,
+        type: 'verify',
+        code: Token.generateCode(),
+        expiresAt: new Date(),
+      });
+
+      await request
+        .put(`${BASE_URL}/me/verify/${code.code}`)
+        .auth(token, { type: 'bearer' })
+        .expect(400)
+        .then((response) => {
+          expect(response.body.message).toEqual('Invalid code');
+        });
+    });
+
+    it('PUT /me/verify/:code should verify user ', async () => {
+      await Token.destroy({ where: { userId: user.id, type: 'verify' } });
+
+      const code = await user.createToken({
+        type: 'verify',
+        code: Token.generateCode(),
+        expiresAt: Token.getExpiration(),
+      });
+
+      await request
+        .put(`${BASE_URL}/me/verify/${code.code}`)
+        .auth(token, { type: 'bearer' })
+        .expect(200);
+
+      const retrievedUser = await User.findOne({ where: { email: data.email } });
+      expect(retrievedUser?.status).toEqual('active');
+    });
   });
 });
