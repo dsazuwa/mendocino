fc8e3b748918df4153ec48b1d9b403aede40c711
dosazuwa <dsazuwa@gmail.com>
2023-09-04 19:53:44 +0100

    test: jwt extract from cookie middleware

---
+++ tests/modules/user/middleware/jwt-extractor.middleware.test.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express';
+
+import { extractJwtFromCookie } from '@App/modules/user';
+
+describe('extractJwtFromCookie Middleware', () => {
+  const res = {} as unknown as Response;
+
+  it('should set Authorization header from access-token cookie', () => {
+    const req = {
+      cookies: { 'access-token': 'your-access-token' },
+      headers: { authorization: '' },
+    } as unknown as Request;
+
+    const next = jest.fn();
+
+    extractJwtFromCookie(req, res, next);
+
+    expect(req.headers.authorization).toEqual('Bearer your-access-token');
+    expect(next).toHaveBeenCalled();
+  });
+
+  it('should not set Authorization header if access-token cookie is missing', () => {
+    const req = {
+      cookies: {},
+      headers: { authorization: '' },
+    } as unknown as Request;
+
+    const next = jest.fn();
+
+    extractJwtFromCookie(req, res, next);
+
+    expect(req.headers.authorization).toBe('');
+    expect(next).toHaveBeenCalled();
+  });
+});
