e829f21b07ecb64dbfe7c0a99c2ff6c77c62f47e
dosazuwa <dsazuwa@gmail.com>
2024-03-20 19:20:55 +0100

    create login page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+
+import { AuthLayout } from '@/components/layout';
+import { buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import LoginForm from './login-form';
+
+export default function Login() {
+  return (
+    <AuthLayout>
+      <h1 className='md-2 text-xl font-bold'>Welcome Back</h1>
+
+      <LoginForm />
+
+      <span className='flex flex-row gap-1 text-xs'>
+        <span>Don&apos;t have an account?</span>
+
+        <Link
+          href='/register'
+          className={cn(
+            buttonVariants({
+              variant: 'primaryLink',
+              size: 'none',
+              className: 'ml-auto text-xs',
+            }),
+          )}
+        >
+          Create an account
+        </Link>
+      </span>
+    </AuthLayout>
+  );
+}
--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -20,6 +20,7 @@ const buttonVariants = cva(
         ghost:
           'hover:bg-neutral-200 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50',
         link: 'text-neutral-900 underline-offset-4 hover:underline dark:text-neutral-50',
+        primaryLink: 'text-primary-700 hover:underline',
       },
       size: {
         default: 'h-9 px-4 py-2',
