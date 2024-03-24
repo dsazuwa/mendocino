073dae7f318dabc495ab2f5eff78acd39286748f
dosazuwa <dsazuwa@gmail.com>
2024-03-24 21:19:41 +0100

    create MUI icon components

--- app/_components/icons/index.tsx
+++ app/_components/icons/index.tsx
@@ -1,21 +1,31 @@
 import AddPerson from './add-person';
 import CircleUser from './circle-user';
+import Cutlery from './cutlery';
 import Home from './home';
 import Info from './info';
 import LogIn from './log-in';
 import LogOut from './log-out';
 import Menu from './menu';
+import People from './people';
 import Receipt from './receipt';
+import ShortReceipt from './receipt-short';
 import Sandwich from './sandwich';
+import Settings from './settings';
+import Store from './store';
 
 export {
   AddPerson,
   CircleUser,
+  Cutlery,
   Home,
   Info,
   LogIn,
   LogOut,
   Menu,
+  People,
   Receipt,
   Sandwich,
+  Settings,
+  ShortReceipt,
+  Store,
 };
