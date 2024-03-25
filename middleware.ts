bf787c2e03a05360b4d89a1090d0363b900aa5e3
dosazuwa <dsazuwa@gmail.com>
2024-03-25 21:35:51 +0100

    protect routes in middleware.js

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+const protectedRoutes: string[] = ['/account'];
+const publicOnlyRoutes = ['/login', '/register', '/recover'];
+
+export default function middleware(request: NextRequest) {
+  const isAuthenticated = !!request.cookies.get('auth-flag')?.value;
+  const { origin, pathname } = request.nextUrl;
+
+  if (!isAuthenticated && protectedRoutes.includes(pathname)) {
+    const absoluteURL = new URL('/', origin); // TODO: store the return path in query params. Eg- /login?redirect=%2Faccount
+    return NextResponse.redirect(absoluteURL.toString());
+  }
+
+  if (isAuthenticated && publicOnlyRoutes.includes(pathname)) {
+    const absoluteURL = new URL('/', origin);
+    return NextResponse.redirect(absoluteURL.toString());
+  }
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
