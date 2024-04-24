ddc05e6196aac7abfa648df81204dfee20800201
dosazuwa <dsazuwa@gmail.com>
2024-04-24 21:40:20 +0100

    refine get address query

--- src/modules/user/services/guest.service.ts
+++ src/modules/user/services/guest.service.ts
@@ -10,28 +10,22 @@ const guestService = {
   getAddresses: async (guestId: string) => {
     const query = `
       SELECT 
-        jsonb_agg(jsonb_build_object(
-          'id', a.address_id,
-          'placeId', a.place_id,
-          'suite', a.suite,
-          'name', a.name,
-          'address', a.address,
-          'zipCode', a.zip_code,
-          'lat', a.lat,
-          'lng', a.lng
-        )) AS addresses
+        a.address_id AS id,
+        a.place_id AS "placeId",
+        a.suite,
+        a.name,
+        a.address,
+        a.zip_code AS "zipCode",
+        a.lat,
+        a.lng
       FROM users.addresses a 
       JOIN users.guest_addresses g ON g.address_id = a.address_id
       WHERE g.guest_id = $id;`;
 
-    const result = (await sequelize.query(query, {
+    return (await sequelize.query(query, {
       type: QueryTypes.SELECT,
       bind: { id: guestId },
-    })) as { addresses?: AddressType & { id: number }[] }[];
-
-    const { addresses } = result[0];
-
-    return addresses?.length ? addresses : [];
+    })) as AddressType & { id: number }[];
   },
 
   createAddress: (guestId: string | undefined, address: AddressType) =>
