b166bb8039ae9551647cb5d7871daba8efe5363b
dosazuwa <dsazuwa@gmail.com>
2024-05-16 19:06:40 +0100

    remove semicolon in transition style

--- packages/next-app/components/mui-tabs/tabs.tsx
+++ packages/next-app/components/mui-tabs/tabs.tsx
@@ -691,7 +691,7 @@ const Tabs = React.forwardRef<HTMLDivElement, TabsProps>(
             ownerState={ownerState}
             className={TabIndicatorProps.className}
             style={{
-              transition: 'all 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;',
+              transition: 'all 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
               ...indicatorStyle,
               ...TabIndicatorProps.style,
             }}
