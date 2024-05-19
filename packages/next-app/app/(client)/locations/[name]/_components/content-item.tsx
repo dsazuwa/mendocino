eecc504f83233ddd74eda3c8ec0df4ee29601dd9
dosazuwa <dsazuwa@gmail.com>
2024-05-19 18:35:25 +0100

    use Intersection Observer to display name on scroll

--- packages/next-app/app/(client)/locations/[name]/_components/content-item.tsx
+++ packages/next-app/app/(client)/locations/[name]/_components/content-item.tsx
@@ -1,9 +1,11 @@
 import { Cross2Icon } from '@radix-ui/react-icons';
+import { useEffect, useState } from 'react';
 
 import ContentHeader from '@/components/content-header';
 import { Button } from '@/components/ui/button';
 import { DialogClose } from '@/components/ui/dialog';
 import { SheetClose } from '@/components/ui/sheet';
+import { cn } from '@/lib/utils';
 import { ItemNode } from '@/redux/slices/order';
 import { Preferences } from './content-preferences';
 import ItemImage from './item-image';
@@ -24,20 +26,62 @@ export default function ItemContent({
   const Comp = isDialog ? DialogClose : SheetClose;
   const { name, description, photoUrl, children } = current;
 
+  const [scrolledPast, setScrolledPast] = useState(false);
+
+  useEffect(() => {
+    const appBar = document.querySelector('#dialog-app-bar');
+    const observedElement = document.querySelector('#scroll-anchor');
+
+    if (appBar == null || observedElement == null) {
+      return;
+    }
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        setScrolledPast(!entries[0].isIntersecting);
+      },
+      { threshold: 0, rootMargin: `-${appBar.clientHeight}px` },
+    );
+
+    observer.observe(observedElement);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
     <>
-      <ContentHeader className='border-none'>
+      <ContentHeader
+        id='dialog-app-bar'
+        className={cn('gap-2 border-none', {
+          'border-b border-solid border-neutral-200': scrolledPast,
+        })}
+      >
         <Comp asChild>
-          <Button variant='ghost' size='icon' className='ml-[-8px]'>
+          <Button
+            variant='ghost'
+            size='icon'
+            className='inlien-flex ml-[-8px] gap-4'
+          >
             <Cross2Icon className='h-4 w-4 fill-neutral-500' />
 
             <span className='sr-only'>Close</span>
           </Button>
         </Comp>
+
+        <div className='line-clamp-1 text-sm font-semibold text-black transition ease-in-out'>
+          {scrolledPast && name}
+        </div>
       </ContentHeader>
 
       <div className='flex flex-col gap-2 overflow-y-auto px-4 pb-4 sm:gap-3.5'>
-        <p className='text-lg font-bold text-black sm:text-2xl'>{name}</p>
+        <p
+          id='scroll-anchor'
+          className='text-lg font-bold text-black sm:text-2xl'
+        >
+          {name}
+        </p>
 
         {description && (
           <p className='text-xs font-semibold text-neutral-500/95 sm:text-sm'>
